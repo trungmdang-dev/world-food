060e4c7105c6c0bf146948e60b1a0d659e2009d8
Trung Dang <trungdang@tecace.com>
2022-07-06 15:38:17 -0700

    Merged from GitHub.

---
+++ app/src/main/java/com/trungdang/android/worldfood/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.trungdang.android.worldfood
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.trungdang.android.worldfood.ui.theme.WorldFoodTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WorldFoodTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WorldFoodTheme {
+        Greeting("Android")
+    }
+}
